551c0cfafc1ec293bfd0daff21ff98bbd14f8e20
jazir788 <jazirsx5@gmail.com>
2023-11-10 12:44:25 +0000

    Added responsiveness for tablet, mobile and desktop devices and margin top

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
-
 import './Chef.css';
-
 import { images } from '../../constants';
 import { SubHeading } from '../../components';
-import { MdImageNotSupported } from 'react-icons/md';
 
 const Chef = () => (
   <div className='app__bg app__wrapper section__padding'>
     <div className='app__wrapper_img app_wrapper_img_reverse'>
       <img src={images.chef} alt='chef'/>  
     </div>
-
     <div className='app__wrapper_info'>
       <SubHeading title="Chef's Word" />
-      <h1 className='headtext__cormorant'> What we believe in</h1>
+      <h1 className='headtext__cormorant'>What we believe in</h1>
 
       <div className='app__chef-content'>
         <div className='app__cheff-content-quote'>
@@ -26,11 +22,9 @@ const Chef = () => (
 
       <div className='app__chef-sign'>
         <p>Kevin Luo</p>
-        <p className='p__opensans'> Chef & Founder</p>
+        <p className='p__opensans'>Chef & Founder</p>
         <img src={images.sign} alt='signature' />
       </div>
-
-
     </div>
   </div>
 );
