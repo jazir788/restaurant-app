8119cf62d3f3256e5a427f7e235a0540c727960d
jazir788 <jazirsx5@gmail.com>
2023-11-13 13:00:25 +0000

    Integrated Footer Components into Footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+
+import { images } from '../../constants';
 
 import './Footer.css';
+import { FooterOverlay, Newsletter } from '../../components';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding" id="login">
+    <FooterOverlay />
+    <Newsletter />
   </div>
 );
 
